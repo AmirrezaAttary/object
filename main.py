c00bb9287f2a6caad78b22385b8a498bdeeb9c11
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-01-15 17:10:40 +0330

    start object

---
+++ main.py
@@ -0,0 +1,18 @@
+class Empoloyee:
+    pass
+
+emp1 = Empoloyee()
+
+emp2 = Empoloyee()
+
+print(emp1 ,
+      emp2)
+
+emp1.name = 'ali'
+emp1.age = 29
+emp2.name = 'hasan'
+emp2.age = 30
+
+print(emp1.name , emp1.age)
+
+print(emp2.name , emp2.age)
