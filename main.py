26f4adc44d15bce96dcec4f66b27ec272399741b
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-01-15 18:07:51 +0330

    majic method

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 class Empoloyee:
+    salery = 3000
+    def __init__(self, name, age):
+      self.name = name
+      self.age = age
+    
+    def hello(self):
+        return 'hello'
+    
+    
+    def __str__(self):
+       return self.name
+   
+    def __repr__(self):
+       return self.__class__.__name__
+   
+    def __add__(self, other):
+        return self.salery + other.salery
+      
+emp1 = Empoloyee('amir',29)
+emp2 = Empoloyee('ali',30)
+
+print(emp1)
+print(emp2.__repr__())
+print(emp1+emp2)
